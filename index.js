b42e34bccdea736e1c9f3633bc8cf70a5f7c4432
EasyJake <EasyJake@users.noreply.github.com>
2023-10-05 00:07:12 -0700

    complete

--- index.js
+++ index.js
@@ -5,7 +5,41 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+// await npc.walkEast(2000)
+// npc.walkSouth(500)
+
+
+// async function moveNPC(){
+//     await npc.walkEast(2000)
+//     npc.walkSouth(500)
+// }
+
+// async function moveNPC(){
+//     await npc.walkNorth(1400);
+//     await npc.walkEast(1200);
+//     await npc.walkSouth(300);
+//     await npc.walkEast(1500);
+//     await npc.walkSouth(1500);
+//     await npc.walkWest(2700);
+//     await npc.walkNorth(400);
+// }
+
+
+// Using the while(true) loop ensures that the NPC will keep following the same path in an infinite loop.
+async function moveNPC(){
+    while(true) {
+        await npc.walkNorth(1400);
+        await npc.walkEast(1200);
+        await npc.walkSouth(300);
+        await npc.walkEast(1500);
+        await npc.walkSouth(1500);
+        await npc.walkWest(2700);
+        await npc.walkNorth(400);
+    }
+}
+
+
+moveNPC()
 
 // Create the inventory
 const inventory = newInventory()
